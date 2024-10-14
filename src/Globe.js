09054d6d8b4299d1e5532d1f1f77549299d3f701
diegocaaas <diego.castaneda1_@hotmail.com>
2024-10-13 20:05:05 -0400

    add mouse scroll to change 3D zoom

--- src/Globe.js
+++ src/Globe.js
@@ -3,27 +3,37 @@ import * as THREE from 'three';
 
 const Globe = () => {
     const mousePositionRef = useRef({ x: 0, y: 0 });
+    const scrollPositionRef = useRef(5);
     const globePressRef = useRef(false);
     const mountRef = useRef(null); 
     const animationRef = useRef();
-    const timeoutRef = useRef(null);
+    const mouseMovementTimeoutRef = useRef(null);
 
     const onMouseMove = (event) => {
         mousePositionRef.current = {x: event.movementX, y: event.movementY};
 
-        if (timeoutRef.current) {
-            clearTimeout(timeoutRef.current);
+        if (mouseMovementTimeoutRef.current) {
+            clearTimeout(mouseMovementTimeoutRef.current);
         }
       
-        timeoutRef.current = setTimeout(() => {
+        mouseMovementTimeoutRef.current = setTimeout(() => {
             mousePositionRef.current = { x: 0, y: 0 };
         }, 1);
     }
 
+    const onMouseScroll = (event) => {
+        if(event.deltaY > 3 && scrollPositionRef.current < 10){
+            scrollPositionRef.current += 0.5;
+        }
+        else if(event.deltaY < 0  && scrollPositionRef.current > 2){
+            scrollPositionRef.current -= 0.5;
+        }
+    }
+
     useEffect(() => {
         const scene = new THREE.Scene();
+        scene.background = new THREE.Color(0xFFFFFF);
         const currentmount = mountRef.current;
-
 
         const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
         camera.position.z = 5; 
@@ -52,9 +62,10 @@ const Globe = () => {
                 const magnitude = Math.sqrt(mousePositionRef.current.x**2 + mousePositionRef.current.y**2);
                 const normalizedX = (mousePositionRef.current.x / magnitude);
                 const normalizedY = (mousePositionRef.current.y / magnitude); 
-                globe.rotation.y += 0.3*normalizedX;
-                globe.rotation.x += 0.3*normalizedY;
+                globe.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), 0.3*normalizedX);
+                globe.rotateOnWorldAxis(new THREE.Vector3(1, 0, 0), 0.3*normalizedY);
             }
+            camera.position.z = scrollPositionRef.current;
             renderer.render(scene, camera); 
         };
         animate(); 
@@ -68,7 +79,13 @@ const Globe = () => {
         };
     }, []); 
 
-    return <div onMouseDown={() => globePressRef.current = true} onMouseUp={() => globePressRef.current = false} onMouseMove={onMouseMove} ref={mountRef} />; // Render the ref div
+    return <div 
+            onMouseDown={() => globePressRef.current = true} 
+            onMouseUp={() => globePressRef.current = false} 
+            onMouseMove={onMouseMove}
+            onWheel={onMouseScroll}  
+            ref={mountRef} 
+        />; 
 };
 
 export default Globe;
