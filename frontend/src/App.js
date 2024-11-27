a803c3348e9bc35dc44264443b028eafe21ea21d
diegocaaas <diego.castaneda1_@hotmail.com>
2024-11-26 22:44:05 -0500

    reformat folders

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Globe from './Globe';
+import InfoPanel from './InfoPanel';
+
+function App() {
+  return (
+    <div className="App">
+      <InfoPanel/>
+      <Globe/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/InfoPanel.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const InfoPanel = () => {
+
+    return <div className="InfoPanel">
+        <h1>
+            Ancient Civilization Explorer
+        </h1>
+        <form>
+            <input>
+            </input>
+        </form>
+    </div>
+}
+
+export default InfoPanel;
